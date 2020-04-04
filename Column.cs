5c8349532aeda140c19347767d2471a20acbbf29
denys.isaiev <denys.isaev@easymorph.com>
2020-04-04 12:17:31 +0300

    Initial commit

---
+++ Column.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace BackgroundGCSuspendsThreads
+{
+    public class Cell
+    {
+        public decimal Value { get; }
+
+        public Cell(decimal value)
+        {
+            Value = value;
+        }
+    }
+
+    public class Column
+    {
+        private readonly Cell[] _vocabulary;
+        private readonly int[] _vector;
+
+        public Column(Cell[] vocabulary, int[] vector)
+        {
+            _vocabulary = vocabulary;
+            _vector = vector;
+        }
+    }
+    
+    public class ColumnBuilder
+    {
+        readonly Dictionary<decimal, int> _dict = new Dictionary<decimal, int>();
+        private int _currentIndex = 0;
+        private readonly List<int> _vector = new List<int>();
+
+        public void Add(decimal value)
+        {
+            if (_dict.TryGetValue(value, out var index))
+                _vector.Add(index);
+            else
+            {
+                _dict.Add(value, _currentIndex);
+                _vector.Add(_currentIndex);
+                _currentIndex++;
+            }
+        }
+
+        public Column ToColumn()
+        {
+            var vocabulary = new Cell[_dict.Count];
+            foreach (var cellPair in _dict)
+            {
+                vocabulary[cellPair.Value] = new Cell(cellPair.Key);
+            }
+
+            return new Column(vocabulary, _vector.ToArray());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackgroundGCSuspendsThreads
+{
+    internal class Program
+    {
+        private static decimal[] decimals;
+        private static decimal[] decimals1;
+        private static string[] strings;
+        private static bool[] bools;
+        private static Cell[] cells;
+        private static Cell[] cells2;
+        private static Cell[] cells3;
+        
+        public static void Main(string[] args)
+        {
+            const int size = 10_000_000;
+            //Initialize heap with lots of big objects
+            decimals = Enumerable.Range(0, size).Select(Convert.ToDecimal).ToArray();
+            decimals1 = Enumerable.Range(0, size).Select(Convert.ToDecimal).ToArray();
+            strings = Enumerable.Range(0, size).Select(e => e.ToString()).ToArray();
+            strings = Enumerable.Range(0, size).Select(e => e.ToString()).ToArray();
+            bools = Enumerable.Range(0, size).Select(e => e % 2 == 0).ToArray();
+            cells = decimals.Select(e => new Cell(e)).ToArray();
+            cells2 = decimals.Select(e => new Cell(e)).ToArray();
+            cells3 = decimals.Select(e => new Cell(e)).ToArray();
+            
+            GC.Collect();
+            
+            Task<Column> job1 = Task.Run(() => Job(size));
+            Task<Column> job2 = Task.Run(() => Job(size));
+            Task.WaitAll(job1, job2);
+        }
+
+        static Column Job(int size)
+        {
+            var builder = new ColumnBuilder();
+            for (int i = 0; i < size; i++)
+            {
+                builder.Add(decimals[i]);
+            }
+
+            return builder.ToColumn();
+        }
+    }
+}
